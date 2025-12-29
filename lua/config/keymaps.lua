37f84109ad517e7ab31110d848e8ae9787996f3b
Dimi <info@nafnaf.gr>
2025-12-29 20:37:22 +0200

    added SpaceX for chmod +x and executing the code from text pane into terminal below

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -19,3 +19,17 @@ vim.keymap.set("n", "<leader>3p", function()
   vim.cmd("terminal")
   vim.cmd("wincmd k")
 end, { desc = "Open 3-pane layout" })
+
+-- Execute current file in terminal (without moving to it)
+vim.keymap.set("n", "<leader>x", function()
+  local file = vim.fn.expand("%:p")
+  local cmd = string.format("chmod +x %s && %s\n", file, file)
+  -- Find terminal buffer and send command
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.bo[buf].buftype == "terminal" then
+      vim.api.nvim_chan_send(vim.b[buf].terminal_job_id, cmd)
+      return
+    end
+  end
+  vim.notify("No terminal found. Open one with <leader>tt", vim.log.levels.WARN)
+end, { desc = "Execute current file" })
